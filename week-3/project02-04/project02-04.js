3dec22e445c6fad496806e849e5231718223a95b
aleyva7 <aleyva@my365.bellevue.edu>
2025-08-27 15:03:36 -0600

    Update project02-04.js

--- week-3/project02-04/project02-04.js
+++ week-3/project02-04/project02-04.js
@@ -9,47 +9,45 @@
       Filename: project02-04.js
  */
 
-const CHICKEN_PRICE = 10.95; //Cost of Chicken
-const HALIBUT_PRICE = 13.95; //Cost of Halibut
-const BURGER_PRICE = 9.95; //Cost of Burger
-const SALMON_PRICE = 18.95; //Cost of Salmon
-const SALAD_PRICE = 7.95; //Cost of Salad
-const SALES_TAX = 0.07; //Sales Tax amount
+  const CHICKEN_PRICE = 10.95; //Cost of Chicken
+  const HALIBUT_PRICE = 13.95; //Cost of Halibut
+  const BURGER_PRICE = 9.95; //Cost of Burger
+  const SALMON_PRICE = 18.95; //Cost of Salmon
+  const SALAD_PRICE = 7.95; //Cost of Salad
+  const SALES_TAX = 0.07; //Sales Tax amount
 
-document.getElementById ("chicken").onclick = calcTotal;
-document.getElementById ("halibut").onclick = calcTotal;
-document.getElementById ("burger").onclick = calcTotal;
-document.getElementById ("salmon").onclick = calcTotal;
-document.getElementById ("salad").onclick = calcTotal;
+  document.getElementById('chicken').onclick = calcTotal;
+  document.getElementById('halibut').onclick = calcTotal;
+  document.getElementById('burger').onclick = calcTotal;
+  document.getElementById('salmon').onclick = calcTotal;
+  document.getElementById('salad').onclick = calcTotal;
 
-function calcTotal () {
-  let cost = 0;
+  function calcTotal() {
+    let cost = 0;
 
-  const buyChicken = document.getElementById ("chicken").checked;
-  const buyHalibut = document.getElementById ("halibut").checked;
-  const buyBurger = document.getElementById ("burger").checked;
-  const buySalmon = document.getElementById ("salmon").checked;
-  const buySalad = document.getElementById ("salad").checked;
+  const buyChicken = document.getElementById('chicken').checked;
+  const buyHalibut = document.getElementById('halibut').checked;
+  const buyBurger = document.getElementById('burger').checked;
+  const buySalmon = document.getElementById('salmon').checked;
+  const buySalad = document.getElementById('salad').checked;
 
-  cost += buyChicken ? CHICKEN_PRICE: 0;
-  cost += buyHalibut ? HALIBUT_PRICE: 0;
-  cost += buyBurger ? BURGER_PRICE: 0;
-  cost += buySalmon ? SALMON_PRICE: 0;
-  cost += buySalad ? SALAD_PRICE: 0;
+  cost += buyChicken ? CHICKEN_PRICE : 0;
+  cost += buyHalibut ? HALIBUT_PRICE : 0;
+  cost += buyBurger ? BURGER_PRICE : 0;
+  cost += buySalmon ? SALMON_PRICE : 0;
+  cost += buySalad ? SALAD_PRICE : 0;
 
-  document.getElementById ("foodTotal").innerHTML = formatCurrency (cost);
+  document.getElementById('foodTotal').innerHTML = formatCurrency(cost);
 
   let tax = cost * SALES_TAX;
-  document.getElementById ("foodTax").innerHTML = formatCurrency (tax);
+  document.getElementById('foodTax').innerHTML = formatCurrency(tax);
 
   let totalCost = cost + tax;
-  document.getElementById ("totalBill").innerHTML = formatCurrency (totalCost);
+  document.getElementById('totalBill').innerHTML = formatCurrency(totalCost);
 }
 
-
-
-
 // Function to display a numeric value as a text string in the format $##.##
- function formatCurrency(value) {
-    return "$" + value.toFixed(2);
- }
+  function formatCurrency(value) {
+    return '$' + value.toFixed(2);
+}
+
