bb4e1fe8149273c079e80b7b79626930d0312327
aleyva7 <aleyva@my365.bellevue.edu>
2025-09-07 11:19:29 -0600

    week 4 upload

---
+++ week-4/chapter/js03_txt.js
@@ -0,0 +1,15 @@
+/*    JavaScript 7th Edition
+     Chapter 3
+     Chapter case
+
+     Tipton Turbines
+     Program to display games results in a web table
+     Author: Amanda Leyva
+     Date:   9/7/25
+
+     Filename: js03.js
+ */
+
+      //Days of the week
+     let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
---
+++ week-4/project03-01/project03-01_txt.js
@@ -0,0 +1,19 @@
+/*    JavaScript 7th Edition
+      Chapter 3
+      Project 03-01
+
+      Application to calculate total order cost
+      Author: Amanda Leyva
+      Date:   9/7/25
+
+      Filename: project03-01.js
+*/
+
+
+
+
+
+ // Function to display a numeric value as a text string in the format $##.##
+ function formatCurrency(value) {
+    return "$" + value.toFixed(2);
+ }
